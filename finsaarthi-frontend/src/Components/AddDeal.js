9fa7cd41b731a1855f5a42aaf25e710130c660b8
jainalisha14 <alisha@ryaz.io>
2025-06-12 12:37:09 +0530

    Styling changes

--- finsaarthi-frontend/src/Components/AddDeal.js
+++ finsaarthi-frontend/src/Components/AddDeal.js
@@ -13,27 +13,27 @@ const AddDeal = () => {
 
   const [dealData, setDealData] = useState({
     DealDate: "",
-    CompanyName: "",
-    Total: 0,
-    GST: "",
-    FreeService: "",
-    Remarks: "",
-    Status: "",
-    UserId: userId,
-    Agent: "",
-    Commission: 0,
     Tentative_Delivery_Date: "",
-    Interest: 0,
-    Machine_Warranty: "",
-    Accessories: "",
-    Others: "",
-    Quantity: 0,
+    CompanyName: "",
+    Agent: "",
     Invoice_Value: 0,
+    GST: "",
+    Total: 0,
     Cash: 0,
     Bank: 0,
     Booking_Amt: 0,
     Before_Delivery: 0,
-    Balance_Payment: 0
+    Balance_Payment: 0,
+    Interest: 0,
+    FreeService: "",
+    Machine_Warranty: "",
+    Accessories: "",
+    Others: "",
+    Remarks: "",
+    Status: "",
+    Commission: 0,
+    Quantity: 0,
+    UserId: userId,
   });
 
   const [names, setNames] = useState([]);
@@ -95,7 +95,7 @@ const AddDeal = () => {
         setTotal(totalSum);
         setDealData((prevData) => ({
           ...prevData,
-          Total: totalSum
+          Total: totalSum,
         }));
       } else {
         console.error("Unexpected response format:", response);
@@ -119,131 +119,183 @@ const AddDeal = () => {
           <>
             <h2>Add New Deal</h2>
             <form onSubmit={handleSubmit} className="modal-form">
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Deal Date</label>
-                  <input type="date" name="DealDate" onChange={handleChange} required />
+              {/* Deal Information */}
+              <fieldset>
+                <legend>Deal Information</legend>
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Deal Date</label>
+                    <input
+                      type="date"
+                      name="DealDate"
+                      onChange={handleChange}
+                      required
+                    />
+                  </div>
+                  <div className="modal-column">
+                    <label>Tentative Delivery Date</label>
+                    <input
+                      type="date"
+                      name="Tentative_Delivery_Date"
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
-                <div className="modal-column">
-                  <label>Company Name</label>
-                  <select name="CompanyName" onChange={handleChange} required>
-                    <option value="">Select Company Name</option>
-                    {names.map((name, index) => (
-                      <option key={index} value={name.CompanyName}>{name.CompanyName}</option>
-                    ))}
-                  </select>
-                </div>
-              </div>
+              </fieldset>
 
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Total</label>
-                  <input type="text" name="Total" value={total} disabled />
+              {/* Client & Agent Info */}
+              <fieldset>
+                <legend>Client & Agent Info</legend>
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Company Name</label>
+                    <select name="CompanyName" onChange={handleChange} required>
+                      <option value="">Select Company Name</option>
+                      {names.map((name, index) => (
+                        <option key={index} value={name.CompanyName}>
+                          {name.CompanyName}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="modal-column">
+                    <label>Agent</label>
+                    <input type="text" name="Agent" onChange={handleChange} />
+                  </div>
                 </div>
-                <div className="modal-column">
-                  <label>GST</label>
-                  <input type="number" name="GST" onChange={handleChange} />
-                </div>
-              </div>
+              </fieldset>
 
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Free Service</label>
-                  <input type="text" name="FreeService" onChange={handleChange} />
+              {/* Financial Details */}
+              <fieldset>
+                <legend>Financial Details</legend>
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Invoice Value</label>
+                    <input
+                      type="number"
+                      name="Invoice_Value"
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="modal-column">
+                    <label>GST</label>
+                    <input type="number" name="GST" onChange={handleChange} />
+                  </div>
                 </div>
-                <div className="modal-column">
-                  <label>Status</label>
-                  <input type="text" name="Status" onChange={handleChange} required />
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Total</label>
+                    <input type="text" name="Total" value={total} disabled />
+                  </div>
                 </div>
-              </div>
+              </fieldset>
 
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Remarks</label>
-                  <textarea name="Remarks" onChange={handleChange}></textarea>
+              {/* Payment Breakdown */}
+              <fieldset>
+                <legend>Payment Breakdown</legend>
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Cash</label>
+                    <input type="number" name="Cash" onChange={handleChange} />
+                  </div>
+                  <div className="modal-column">
+                    <label>Bank</label>
+                    <input type="number" name="Bank" onChange={handleChange} />
+                  </div>
                 </div>
-              </div>
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Booking Amount</label>
+                    <input
+                      type="number"
+                      name="Booking_Amt"
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="modal-column">
+                    <label>Before Delivery</label>
+                    <input
+                      type="number"
+                      name="Before_Delivery"
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Balance Payment</label>
+                    <input
+                      type="number"
+                      name="Balance_Payment"
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
+              </fieldset>
 
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Agent</label>
-                  <input type="text" name="Agent" onChange={handleChange} />
+              {/* Additional Charges & Terms */}
+              <fieldset>
+                <legend>Additional Charges & Terms</legend>
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Interest</label>
+                    <input
+                      type="number"
+                      name="Interest"
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="modal-column">
+                    <label>Free Service</label>
+                    <input
+                      type="text"
+                      name="FreeService"
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
-                <div className="modal-column">
-                  <label>Commission</label>
-                  <input type="number" name="Commission" onChange={handleChange} />
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Machine Warranty</label>
+                    <input
+                      type="text"
+                      name="Machine_Warranty"
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className="modal-column">
+                    <label>Accessories</label>
+                    <input
+                      type="text"
+                      name="Accessories"
+                      onChange={handleChange}
+                    />
+                  </div>
                 </div>
-              </div>
+              </fieldset>
 
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Tentative Delivery Date</label>
-                  <input type="date" name="Tentative_Delivery_Date" onChange={handleChange} />
+              {/* Miscellaneous */}
+              <fieldset>
+                <legend>Miscellaneous</legend>
+                <div className="modal-row">
+                  <div className="modal-column">
+                    <label>Others</label>
+                    <input type="text" name="Others" onChange={handleChange} />
+                  </div>
+                  <div className="modal-column">
+                    <label>Remarks</label>
+                    <textarea name="Remarks" onChange={handleChange}></textarea>
+                  </div>
                 </div>
-                <div className="modal-column">
-                  <label>Interest</label>
-                  <input type="number" name="Interest" onChange={handleChange} />
-                </div>
-              </div>
-
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Machine Warranty</label>
-                  <input type="text" name="Machine_Warranty" onChange={handleChange} />
-                </div>
-                <div className="modal-column">
-                  <label>Accessories</label>
-                  <input type="text" name="Accessories" onChange={handleChange} />
-                </div>
-              </div>
-
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Others</label>
-                  <input type="text" name="Others" onChange={handleChange} />
-                </div>
-                <div className="modal-column">
-                  <label>Quantity</label>
-                  <input type="number" name="Quantity" onChange={handleChange} />
-                </div>
-              </div>
-
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Invoice Value</label>
-                  <input type="number" name="Invoice_Value" onChange={handleChange} />
-                </div>
-                <div className="modal-column">
-                  <label>Cash</label>
-                  <input type="number" name="Cash" onChange={handleChange} />
-                </div>
-              </div>
-
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Bank</label>
-                  <input type="number" name="Bank" onChange={handleChange} />
-                </div>
-                <div className="modal-column">
-                  <label>Booking Amount</label>
-                  <input type="number" name="Booking_Amt" onChange={handleChange} />
-                </div>
-              </div>
-
-              <div className="modal-row">
-                <div className="modal-column">
-                  <label>Before Delivery</label>
-                  <input type="number" name="Before_Delivery" onChange={handleChange} />
-                </div>
-                <div className="modal-column">
-                  <label>Balance Payment</label>
-                  <input type="number" name="Balance_Payment" onChange={handleChange} />
-                </div>
-              </div>
+              </fieldset>
 
               <div className="button-container">
                 <button type="submit">Add Deal</button>
-                <button type="button" className="close-modal-btn" onClick={handleClose}>
+                <button
+                  type="button"
+                  className="close-modal-btn"
+                  onClick={handleClose}
+                >
                   Close
                 </button>
               </div>
